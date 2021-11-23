f6b6dd4bc8dff8d2e6c5ba03c9225d859310bc7d
Hendrik Mennen <info@mennen.online>
2021-11-23 12:30:53 +0100

    Added Server Observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,11 +7,13 @@ use App\Models\CustomerContact;
 use App\Models\CustomerInvoice;
 use App\Models\CustomerProduct;
 use App\Models\Domain;
+use App\Models\Server;
 use App\Observers\CustomerContactObserver;
 use App\Observers\CustomerInvoiceObserver;
 use App\Observers\CustomerObserver;
 use App\Observers\CustomerProductObserver;
 use App\Observers\DomainObserver;
+use App\Observers\ServerObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -34,6 +36,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Domain::observe(DomainObserver::class);
+        Server::observe(ServerObserver::class);
         Customer::observe(CustomerObserver::class);
         CustomerContact::observe(CustomerContactObserver::class);
         CustomerProduct::observe(CustomerProductObserver::class);
