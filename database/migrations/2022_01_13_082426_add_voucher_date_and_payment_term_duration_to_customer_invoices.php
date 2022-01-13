a99dc65abde2d2687f712407fd92fabc12323a84
Hendrik Mennen <info@mennen.online>
2022-01-13 17:54:45 +0100

    Fixed Migration

--- database/migrations/2022_01_13_082426_add_voucher_date_and_payment_term_duration_to_customer_invoices.php
+++ database/migrations/2022_01_13_082426_add_voucher_date_and_payment_term_duration_to_customer_invoices.php
@@ -24,8 +24,8 @@ class AddVoucherDateAndPaymentTermDurationToCustomerInvoices extends Migration
         });
         Schema::table('customer_invoices', function (Blueprint $table) {
             $table->renameColumn('voucherNumber', 'voucher_number');
-            $table->integer('payment_term_duration')->nullable()->default(null)->after('totalTaxAmount');
-            $table->date('voucher_date')->nullable()->default(null)->after('totalTaxAmount');
+            $table->integer('payment_term_duration')->nullable()->default(null)->after('total_tax_amount');
+            $table->date('voucher_date')->nullable()->default(null)->after('total_tax_amount');
         });
     }
 
