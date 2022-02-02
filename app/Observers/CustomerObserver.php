2ccac99be64edd4c0fcc791cd0f8ae8e97cab397
Hendrik Mennen <hendrik.mennen@mennen.online>
2022-02-02 09:12:14 +0100

    Moved updating and creating actions from Customer.php in CustomerObserver.php

--- app/Observers/CustomerObserver.php
+++ app/Observers/CustomerObserver.php
@@ -3,9 +3,35 @@
 namespace App\Observers;
 
 use App\Models\Customer;
+use App\Services\Lexoffice\Endpoints\ContactsEndpoint;
 
 class CustomerObserver
 {
+    public function creating(Customer $customer) {
+        if ($customer->customer_type) {
+            $customer->lexoffice_id = match ($customer->customer_type) {
+                'company' => app()->make(ContactsEndpoint::class)->createCompanyContact($customer)->id,
+                'person' => app()->make(ContactsEndpoint::class)->createPersonContact($customer)->id,
+            };
+        }
+
+        if ($customer->customer_type === 'company') {
+            $customer->company = [
+                'allowTaxFreeInvoices' => $customer->allowTaxFreeInvoices,
+                'name' => $customer->companyName,
+                'taxNumber' => $customer->taxNumber,
+                'vatRegistrationId' => $customer->vatRegistrationId
+            ];
+        }
+
+        $fillableFields = $customer->getFillable();
+        foreach ($customer->attributes as $attribute => $value) {
+            if (!in_array($attribute, $fillableFields)) {
+                unset($customer->attributes[$attribute]);
+            }
+        }
+    }
+
     /**
      * Handle the Customer "created" event.
      *
@@ -14,6 +40,30 @@ class CustomerObserver
      */
     public function created(Customer $customer)
     {
+    }
+
+
+    public function updating(Customer $customer) {
+        if (!empty($customer->street_number)
+            && !empty($customer->postcode)
+            && !empty($customer->city)
+            && !empty($customer->countryCode)) {
+            app()->make(ContactsEndpoint::class)->createOrUpdateCompanyBillingAddress(
+                $customer,
+                $customer->supplement ?? '',
+                $customer->street_number,
+                $customer->postcode,
+                $customer->city,
+                $customer->countryCode
+            );
+        }
+
+        $fillableFields = $customer->getFillable();
+        foreach ($customer->attributes as $attribute => $value) {
+            if (!in_array($attribute, $fillableFields)) {
+                unset($customer->attributes[$attribute]);
+            }
+        }
     }
 
     /**
